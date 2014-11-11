317443bacdb00ace96c0da8c3abde0372c409f50
justinimel <justinimel@users.noreply.github.com>
2014-11-10 22:47:45 -0600

    Update CDVBackgroundLocationEnabler.m

--- src/ios/CDVBackgroundLocationEnabler.m
+++ src/ios/CDVBackgroundLocationEnabler.m
@@ -9,8 +9,6 @@
 {
     CLLocationManager* locationManager = [[CLLocationManager alloc] init];
     locationManager.pausesLocationUpdatesAutomatically = NO;
-    locationManager.distanceFilter = 10;
-    locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
     [locationManager startUpdatingLocation];
 }
 
