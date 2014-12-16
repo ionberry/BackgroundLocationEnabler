d5622de9a7d428b03eb1ec1b38b154b6cf19508d
justinimel <justinimel@users.noreply.github.com>
2014-12-15 21:11:46 -0600

    import?

--- src/ios/CDVLocation+Unlimited.h
+++ src/ios/CDVLocation+Unlimited.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <Cordova/CDVPlugin.h>
+#import <Cordova/CDVLocation.h>
 #import <Foundation/Foundation.h>
 
 @interface CDVLocation(Unlimited)
