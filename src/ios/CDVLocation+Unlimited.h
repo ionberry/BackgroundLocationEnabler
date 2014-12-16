12c65142ca92696ac68f2c8b3bce4fc09cd0a6e9
justinimel <jimel@Justins-MacBook-Pro.local>
2014-12-15 21:18:47 -0600

    Revert 4cb7cc6..f81ebde
    This rolls back to commit 4cb7cc61358302deb0280ccf4eb9c3fb918bfdc0.

--- src/ios/CDVLocation+Unlimited.h
+++ src/ios/CDVLocation+Unlimited.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
-#import <CordovaLib/Classes/CDVPlugin.h>
+#import <Cordova/CDVPlugin.h>
 #import <Foundation/Foundation.h>
 
 @interface CDVLocation(Unlimited)
